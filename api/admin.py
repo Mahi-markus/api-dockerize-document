4df0febdedcfd519f5c2a0be65bd7e71cdce084d
Mahi-marcus <mrahman61142@gmail.com>
2025-01-28 17:35:43 +0600

    database add

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import HelloWorld
 
-# Register your models here.
+class HelloWorldAdmin(admin.ModelAdmin):
+    # Display the 'id' and 'message' fields in the admin list view
+    list_display = ('id', 'message')
+    
+    # Enable search functionality for the 'message' field
+    search_fields = ('message',)
+    
+    # Enable filtering based on the 'message' field
+    list_filter = ('message',)
+
+# Register the HelloWorld model with the custom admin configuration
+admin.site.register(HelloWorld, HelloWorldAdmin)
